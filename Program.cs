ec9ae593cfc98bdfcee33c057519892ce79f6e6a
unknown <xfvjykk@yandex.ru>
2022-11-26 13:17:24 +0300

    2

--- Program.cs
+++ Program.cs
@@ -11,3 +11,32 @@ while (number1 < 100)
 int otvet1 = (number1 % 100) / 10;
 Console.WriteLine(otvet1);
 
+
+
+
+
+Console.WriteLine("Введите число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+  string num = number2.ToString();
+if (num[2] > 0)
+{
+    Console.WriteLine (num[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
+
+int sum = Convert.ToInt32(Console.ReadLine());
+int (a) = 1;
+if (sum>99)
+{
+    a = (sum % 100) % 10;
+    Console.WriteLine (a);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
