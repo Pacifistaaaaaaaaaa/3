4154b8df3d9887ee912a06830cb69d8f5db253bc
unknown <xfvjykk@yandex.ru>
2022-11-26 12:24:01 +0300

    Коммит

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Введите трехзначное число");
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+while (number1 < 100)
+{
+    Console.WriteLine("Введите трехзначное число");
+    number1 = Convert.ToInt32(Console.ReadLine());
+}
+int otvet1 = (number1 % 100) / 10;
+Console.WriteLine(otvet1);
+
